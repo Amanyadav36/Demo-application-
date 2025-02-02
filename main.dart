29d9be74c4b390118a80060ca7d8cd086881495c
Aman Yadav <112255136+Amanyadav36@users.noreply.github.com>
2025-02-02 14:13:15 +0530

    /libs

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'register.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
